108c09fcda7f5bdbeee4b4207ee15ebd41a479a6
Emmanuel Amicel <emmanuel.amicel@isogeo.fr>
2023-01-08 17:04:51 +0100

    Add first header section

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import styles from '../styles/Header.module.css';
 
-
 export default function Header() {
   return (
     <section id='header' className={styles.section}>
-      <h1>Home</h1>
+      <header className={styles.intro}>
+        <i>Bienvenue sur mon site.<br /> Je m'appelle <b>Emmanuel Amicel</b> et je suis</i>
+      </header>
+      <h1 className={styles.h1}>DevOps</h1>
+      <p className={styles.headerText}>Je suis à la recherche d'un CDI sur <b>Rennes</b> à partir de <b>juin 2023</b>.</p>
+      <ul className={styles.ul}>
+        <li className={styles.li}><img src="sql.png" alt="SQL" className={styles.logo} /></li>
+        <li className={styles.li}><img src="cs.png" alt="C#" className={styles.logo} /></li>
+        <li className={styles.li}><img src="node.png" alt="Node.js" className={styles.logo} /></li>
+        <li className={styles.li}><img src="docker.png" alt="Docker" className={styles.logo} /></li>
+        <li className={styles.li}><img src="azure.png" alt="Azure" className={styles.logo} /></li>
+      </ul>
     </section>
   )
 }
